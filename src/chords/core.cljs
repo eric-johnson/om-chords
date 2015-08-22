0f2bcd1c99546b4c742719957004d72714135933
Eric Johnson <eric@eric-johnson.net>
2015-08-22 15:44:33 -0500

    Done with basic functionality.
    Needs positive number validation, styling, and support for feet & inches.
    Bonus points for breking inputs into components and reducing duplication.

--- src/chords/core.cljs
+++ src/chords/core.cljs
@@ -4,7 +4,23 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:radius 1, :chord-length 0.5}))
+(defonce app-state (atom {:radius 1, :chord-length 0.5, :offset 0}))
+
+(defn diam-to-perim [radius offset]
+  (.sqrt js/Math (- (.pow js/Math radius 2)
+                    (.pow js/Math offset 2))))
+
+(defn diam-to-chord [radius chord-length]
+  (let [half-chord (/ chord-length 2)]
+    (.sqrt js/Math (- (.pow js/Math radius 2)
+                      (.pow js/Math half-chord 2)))))
+
+(defn chord-to-perim [data]
+  (let [radius (js/parseFloat (:radius data))
+        chord-length (js/parseFloat (:chord-length data))
+        offset (js/parseFloat (:offset data))]
+    (- (diam-to-perim radius offset)
+       (diam-to-chord radius chord-length))))
 
 (defn numeric? [text]
   ;; https://github.com/jquery/jquery/blob/2.1.4/src/core.js#L217
@@ -13,26 +29,33 @@
          1)
       0))
 
-(defn handle-change [e owner state key]
-  (let [old (key state)
+(defn handle-change [e data key]
+  (let [old (key data)
         new (.. e -target -value)]
     (if (numeric? new)
-      (om/set-state! owner key new)
-      (om/set-state! owner key old))))
+      (om/transact! data key #(identity new)))))
 
 (defn input-view [data owner]
   (reify
-    om/IRenderState
-    (render-state [_ state]
+    om/IRender
+    (render [_]
       (dom/div nil
                (dom/p nil
                       (dom/label #js {:htmlFor "radius"} "Radius")
-                      (dom/input #js {:type "text" :ref "radius" :id "radius" :value (:radius state)
-                                      :onChange #(handle-change % owner state :radius)}))
+                      (dom/input #js {:type "text" :ref "radius" :id "radius" :value (:radius data)
+                                      :onChange #(handle-change % data :radius)}))
                (dom/p nil
                       (dom/label #js {:htmlFor "chord-length"} "Chord Length")
-                      (dom/input #js {:type "text" :ref "chord-length" :id "chord-length" :value (:chord-length state)
-                                      :onChange #(handle-change % owner state :chord-length)}))))))
+                      (dom/input #js {:type "text" :ref "chord-length" :id "chord-length" :value (:chord-length data)
+                                      :onChange #(handle-change % data :chord-length)}))
+               (dom/p nil
+                      (dom/label #js {:htmlFor "offset"} "Offset from Center")
+                      (dom/input #js {:type "text" :ref "offset" :id "offset" :value (:offset data)
+                                      :onChange #(handle-change % data :offset)}))
+               (dom/p nil
+                      (dom/label #js {:htmlFor "chord-to-perim"} "Chord to Perimeter at offset")
+                      (dom/input #js {:type "text" :ref "chord-to-perim" :id "chord-to-perim"
+                                      :value (chord-to-perim data) :disabled true}))))))
 
 (om/root
   input-view
